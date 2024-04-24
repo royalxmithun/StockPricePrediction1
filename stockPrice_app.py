6bcd4e1f0b06e7fc659c4b381b34c822ecc962e3
royalxmithun <108182355+royalxmithun@users.noreply.github.com>
2024-04-24 10:24:51 +0530

    Update stockPrice_app.py

--- stockPrice_app.py
+++ stockPrice_app.py
@@ -166,7 +166,7 @@ def line_chart(userInput):
     else: # IF NOT EMPTY
         stock_info = get_stock_data(userInput)
 
-        visual_df = pd.DataFrame(stock_info, columns = ['Open', 'Avg Close'])
+        visual_df = pd.DataFrame(stock_info, columns = ['Open', 'Adj Close'])
 
         return visual_df
 
